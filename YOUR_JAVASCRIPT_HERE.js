7dead31d9745edd87218887fcc69e0be0d4b611d
ThelsK <github@thels.nl>
2019-08-09 20:26:14 +0200

    Added event log

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,6 +1,6 @@
 // Write your JS here
 const hero = {
-    name: "Bob",
+    name: "Bob the Brave",
     heroic: true,
     inventory: [],
     health: 10,
@@ -12,7 +12,10 @@ const hero = {
 
 function rest(character) {
     if (character.health == 10) {
-        alert("You're already fully rested!")
+        alert("You are already at full health!")
+    }
+    else {
+        addEvent(`${character.name} rests and regains ${10 - character.health} health.`)
     }
     character.health = 10;
     return character;
@@ -20,10 +23,22 @@ function rest(character) {
 
 function pickUpItem(character, item) {
     character.inventory.push(item)
+    if (item.damage) {
+        addEvent(`${character.name} picks up ${item.type} (${character.weapon.damage} damage).`)
+    }
+    else {
+        addEvent(`${character.name} picks up ${item.type}.`)
+    }    
 }
 
 function equipWeapon(character, item) {
-    if (character.inventory[0]) {
+    if (character.inventory[0] && character.weapon != character.inventory[0]) {
         character.weapon = character.inventory[0]
+        addEvent(`${character.name} equips ${character.weapon.type} (${character.weapon.damage} damage).`)
     }
+}
+
+function addEvent(eventText) {
+    const eventElement = document.getElementById("eventList")
+    eventElement.innerText = `${eventElement.innerText}\n${eventText}`
 }
