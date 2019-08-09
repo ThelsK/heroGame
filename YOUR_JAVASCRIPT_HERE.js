661c1fbcd0ee06d174e528146de035cb68b86d2d
ThelsK <github@thels.nl>
2019-08-09 20:25:32 +0200

    Followed all the instructions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,29 @@
 // Write your JS here
+const hero = {
+    name: "Bob",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "stick",
+        damage: 2,
+    }
+}
+
+function rest(character) {
+    if (character.health == 10) {
+        alert("You're already fully rested!")
+    }
+    character.health = 10;
+    return character;
+}
+
+function pickUpItem(character, item) {
+    character.inventory.push(item)
+}
+
+function equipWeapon(character, item) {
+    if (character.inventory[0]) {
+        character.weapon = character.inventory[0]
+    }
+}
